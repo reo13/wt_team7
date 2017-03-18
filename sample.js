81e237fab074ddc544103ae6f6bcb5953eaf8dde
r15h <rohitkukreja1508@gmail.com>
2017-03-18 16:04:13 +0530

    Reqres

--- sample.js
+++ sample.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+var express = require('express');
+console.log("Hello");
+var server = new express();
+server.get('/',function(req,res){
+	res.send('hello you');
+})
+server.listen(8000,function(){
+console.log('listening on localhost:8000')})
