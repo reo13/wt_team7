0f9cc211aa2bf73117ca92d3712a5368cce7982d
r15h <rohitkukreja1508@gmail.com>
2017-03-18 16:36:06 +0530

    xy

--- sample1.js
+++ sample1.js
@@ -1,3 +1,17 @@
-console.log("Hello world");
-console.log("Hello world again");
-alert("Hello");
+var express = require('express');
+var bodyParser = require('body-parser');
+var server = new express();
+server.use(bodyParser());
+var homeListener = function(req,res){
+  res.send('hello you , welcome to homeListener')
+};
+server.get('/',homeListener);
+server.get('/search', function(req,res){
+ console.log(req.query);	
+ var in_var = Number(req.query.var1) ;
+ res.json({ok:true,message:'Square is '+(in_var*in_var)})
+})
+
+server.listen(8000,function(){
+	console.log('Listening on localhost:8000')
+})
