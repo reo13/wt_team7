36cf4b8c0752fae41f5b72803c147adb72d0227a
Ritesh Surve <Ritesh Surve>
2017-03-18 16:03:50 +0530

    exp

--- sample.js
+++ sample.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+var  express = require('express');
+console.log('hey you!');
+
+var server = new express();
+
+server.get('/',function(req,res)){
+	res.send('hello you ')
+})
+
+server.listen(8000,function(){
+	console.log('Listening on localhost:8000')
+})
